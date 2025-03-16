40b12bf043bb50ca45c4795091e0b1f32f62d5e0
Guilherme <guilhermeppirozi@gmail.com>
2025-03-16 19:05:44 -0300

    fiz o ex11 e atualizei pro jdk 23

--- src/ex11.java
+++ src/ex11.java
@@ -2,8 +2,24 @@ import java.util.Scanner;
 
 public class ex11 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.print("Digite o valor inicial: ");
+        int valorInicial = leitor.nextInt();
+
+        System.out.print("Digite o valor do incremento: ");
+        int incremento = leitor.nextInt();
+
+        System.out.print("Sequência: ");
+        for (int i = valorInicial; ; i += incremento) {
+            System.out.print(i);
+
+            if (i > 100) {
+                break;
+            }
+
+            System.out.print(", ");
+        }
     }
 }
